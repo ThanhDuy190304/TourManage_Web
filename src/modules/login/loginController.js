c4b41695e2f53d6c62d3f863b7f64d7f3b2e8812
Thanh Duy <thanhduy190304ptd@gmail.com>
2025-01-12 11:13:12 +0700

    fix cookie

--- src/modules/login/loginController.js
+++ src/modules/login/loginController.js
@@ -29,14 +29,13 @@ class loginController {
                     path: '/',
                 });
                 if (user.roleId === 1) {
-
                     res.cookie(process.env.ACCESS_TOKEN_NAME, accessToken, {
-                        httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'Strict',
+                        httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'None',
                         path: '/',
                         domain: process.env.NODE_ENV === 'production' ? process.env.ADMIN_DOMAIN : undefined,
                     });
                     res.cookie(process.env.REFRESH_TOKEN_NAME, refreshToken, {
-                        httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'Strict',
+                        httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'None',
                         path: '/',
                         domain: process.env.NODE_ENV === 'production' ? process.env.ADMIN_DOMAIN : undefined,
                     });
